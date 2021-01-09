d0fed2ce42c08c00e48b84aa57e77fc78e901e1a
xieguigang <xie.guigang@gcmodeller.org>
2021-01-09 18:23:25 +0800

    editor components

--- RsharpDev/WinForms/RsharpDevEditor.designer.vb
+++ RsharpDev/WinForms/RsharpDevEditor.designer.vb
@@ -21,63 +21,31 @@ Partial Class RsharpDevEditor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RsharpDevEditor))
-        Me.FastColoredTextBox1 = New FastColoredTextBoxNS.FastColoredTextBox()
-        Me.DocumentMap1 = New FastColoredTextBoxNS.DocumentMap()
-        CType(Me.FastColoredTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Editor1 = New RDev.Editor()
         Me.SuspendLayout()
         '
-        'FastColoredTextBox1
+        'Editor1
         '
-        Me.FastColoredTextBox1.AutoCompleteBracketsList = New Char() {Global.Microsoft.VisualBasic.ChrW(40), Global.Microsoft.VisualBasic.ChrW(41), Global.Microsoft.VisualBasic.ChrW(123), Global.Microsoft.VisualBasic.ChrW(125), Global.Microsoft.VisualBasic.ChrW(91), Global.Microsoft.VisualBasic.ChrW(93), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(34), Global.Microsoft.VisualBasic.ChrW(39), Global.Microsoft.VisualBasic.ChrW(39)}
-        Me.FastColoredTextBox1.AutoIndentCharsPatterns = "^\s*[\w\.]+(\s\w+)?\s*(?<range>=)\s*(?<range>[^;=]+);" & Global.Microsoft.VisualBasic.ChrW(10) & "^\s*(case|default)\s*[^:]*(" &
-    "?<range>:)\s*(?<range>[^;]+);"
-        Me.FastColoredTextBox1.AutoScrollMinSize = New System.Drawing.Size(109, 15)
-        Me.FastColoredTextBox1.BackBrush = Nothing
-        Me.FastColoredTextBox1.CharHeight = 15
-        Me.FastColoredTextBox1.CharWidth = 7
-        Me.FastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.FastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
-        Me.FastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.FastColoredTextBox1.Font = New System.Drawing.Font("Consolas", 9.75!)
-        Me.FastColoredTextBox1.IsReplaceMode = False
-        Me.FastColoredTextBox1.Location = New System.Drawing.Point(0, 0)
-        Me.FastColoredTextBox1.Name = "FastColoredTextBox1"
-        Me.FastColoredTextBox1.Paddings = New System.Windows.Forms.Padding(0)
-        Me.FastColoredTextBox1.SelectionColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.FastColoredTextBox1.ServiceColors = CType(resources.GetObject("FastColoredTextBox1.ServiceColors"), FastColoredTextBoxNS.ServiceColors)
-        Me.FastColoredTextBox1.Size = New System.Drawing.Size(629, 539)
-        Me.FastColoredTextBox1.TabIndex = 0
-        Me.FastColoredTextBox1.Text = "#/usr/bin/R#"
-        Me.FastColoredTextBox1.Zoom = 100
-        '
-        'DocumentMap1
-        '
-        Me.DocumentMap1.Dock = System.Windows.Forms.DockStyle.Right
-        Me.DocumentMap1.ForeColor = System.Drawing.Color.Maroon
-        Me.DocumentMap1.Location = New System.Drawing.Point(629, 0)
-        Me.DocumentMap1.Name = "DocumentMap1"
-        Me.DocumentMap1.Size = New System.Drawing.Size(154, 539)
-        Me.DocumentMap1.TabIndex = 1
-        Me.DocumentMap1.Target = Me.FastColoredTextBox1
-        Me.DocumentMap1.Text = "DocumentMap1"
+        Me.Editor1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Editor1.FilePath = Nothing
+        Me.Editor1.Location = New System.Drawing.Point(0, 0)
+        Me.Editor1.Name = "Editor1"
+        Me.Editor1.Size = New System.Drawing.Size(783, 539)
+        Me.Editor1.TabIndex = 0
         '
         'RsharpDevEditor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(783, 539)
-        Me.Controls.Add(Me.FastColoredTextBox1)
-        Me.Controls.Add(Me.DocumentMap1)
+        Me.Controls.Add(Me.Editor1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "RsharpDevEditor"
         Me.Text = "New Script"
-        CType(Me.FastColoredTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents FastColoredTextBox1 As FastColoredTextBoxNS.FastColoredTextBox
-    Friend WithEvents DocumentMap1 As FastColoredTextBoxNS.DocumentMap
+    Friend WithEvents Editor1 As RDev.Editor
 End Class
--- RsharpDev/WinForms/RsharpDevEditor.vb
+++ RsharpDev/WinForms/RsharpDevEditor.vb
@@ -25,200 +25,29 @@ Public Class RsharpDevEditor : Inherits DockContent
 
     Public ReadOnly Property MimeType As ContentType() Implements IFileReference.MimeType
         Get
-            Return {
-                New ContentType With {.Details = "http://r_lang.dev.smrucc.org/", .FileExt = ".R", .MIMEType = "text/r_sharp", .Name = "R# script"}
-            }
+            Return Editor1.MimeType
         End Get
     End Property
 
     Private Sub RsharpDevEditor_Load(sender As Object, e As EventArgs) Handles Me.Load
         Me.ShowIcon = True
-
-        Dim syntaxHighlighter As New SyntaxHighlighter(FastColoredTextBox1)
-
-        FastColoredTextBox1.Text = "#!/usr/local/bin/R#"
-        FastColoredTextBox1.SyntaxHighlighter = syntaxHighlighter
     End Sub
 
     Public Function View(script As String) As DockContent Implements Viewer.View
         If script.FileExists Then
             FilePath = script
-            script = script.ReadAllText
+            Editor1.LoadScript(script)
             TabText = FilePath.FileName
         End If
-
-        FastColoredTextBox1.Text = script
 
         Return Me
     End Function
 
     Public Function Save(path As String, encoding As Encoding) As Boolean Implements ISaveHandle.Save
-        Return FastColoredTextBox1.Text.SaveTo(path, encoding)
+        Return Editor1.Save(path, encoding)
     End Function
 
     Public Function Save(path As String, Optional encoding As Encodings = Encodings.UTF8) As Boolean Implements ISaveHandle.Save
         Return Save(path, encoding.CodePage)
     End Function
-
-    ''' <summary>
-    ''' 关键词
-    ''' </summary>
-    Dim blue As New TextStyle(Brushes.Blue, Nothing, FontStyle.Regular)
-    ''' <summary>
-    ''' 代码注释
-    ''' </summary>
-    Dim green As New TextStyle(Brushes.Green, Nothing, FontStyle.Italic)
-    ''' <summary>
-    ''' 字符串
-    ''' </summary>
-    Dim red As New TextStyle(Brushes.Brown, Nothing, FontStyle.Bold)
-    ''' <summary>
-    ''' 结束符号
-    ''' </summary>
-    Dim endSymbol As New TextStyle(Brushes.Black, Nothing, FontStyle.Bold)
-    ''' <summary>
-    ''' 内部函数
-    ''' </summary>
-    Dim purple As New TextStyle(Brushes.Purple, Nothing, FontStyle.Regular)
-    ''' <summary>
-    ''' 数字
-    ''' </summary>
-    Dim orange As New TextStyle(Brushes.OrangeRed, Nothing, FontStyle.Bold)
-    Dim link As New TextStyle(Brushes.Blue, Brushes.AliceBlue, FontStyle.Underline Or FontStyle.Bold)
-    Dim colorCode As New TextStyle(Brushes.Red, Brushes.LightGray, fontStyle:=FontStyle.Bold Or FontStyle.Italic Or FontStyle.Underline)
-    Dim pipeLine As New TextStyle(Brushes.Red, Brushes.LightGray, FontStyle.Underline)
-    Dim interpolate As New TextStyle(Brushes.Black, Brushes.AliceBlue, FontStyle.Italic)
-    Dim funcCall As New TextStyle(Brushes.Black, Brushes.AliceBlue, FontStyle.Regular)
-
-    Dim callFunc As String = "[^\s]+\s*\("
-    Dim htmlcolor As String = "[#][0-9a-fA-F]{6}"
-    Dim stringInterpolate As String = "[$]\{.+?\}"
-
-    Dim buildInfunction As String = "\s?(" & {
-        "list", "stop", "print"
-    }.Select(Function(a) $"({a})") _
-     .JoinBy("|") & ")\s*\("
-
-    Dim startKeyword As String = "(let|const|imports|using)\s"
-    Dim keywords As String = "\s(" & {
-        "as", "integer",
-        "from",
-        "in"
-    }.Select(Function(a) $"({a})").JoinBy("|") & ")\s"
-
-    Dim numbers As String = "[-]?\d*(\.\d+)?([Ee][-]?\d+)?"
-    Dim functionKeyword As String = "\s(function|if|for|require)\s*\("
-    Dim keyword2 As String = "\s(" & {
-        "double", "boolean", "string", "integer",
-        "else"
-    }.Select(Function(a) $"({a})") _
-     .JoinBy("|") & ")(\s|\)|,)"
-
-    Private Sub FastColoredTextBox1_ToolTipNeeded(sender As Object, e As ToolTipNeededEventArgs) Handles FastColoredTextBox1.ToolTipNeeded
-        If Not String.IsNullOrEmpty(e.HoveredWord) Then
-            e.ToolTipTitle = e.HoveredWord
-            e.ToolTipText = vbCrLf & GetTooltipContent(Strings.Trim(e.HoveredWord)).DoCall(AddressOf Strings.Trim)
-        End If
-
-        Dim range As New Range(sender, e.Place, e.Place)
-        Dim hoveredWord = range.GetFragment("[^\n]").Text
-
-        e.ToolTipTitle = hoveredWord
-        e.ToolTipText = vbCrLf & GetTooltipContent(Strings.Trim(hoveredWord)).DoCall(AddressOf Strings.Trim)
-    End Sub
-
-    Private Function GetTooltipContent(hoveredWord As String) As String
-        hoveredWord = hoveredWord.Trim(" "c, ASCII.TAB, ASCII.CR, ASCII.LF)
-
-        If hoveredWord.StringEmpty Then
-            Return Nothing
-        End If
-
-        If hoveredWord.First = "}"c AndAlso hoveredWord.Last = "{"c Then
-            hoveredWord = hoveredWord.GetStackValue("}", "{").Trim(" "c, ASCII.TAB, ASCII.CR, ASCII.LF)
-        End If
-
-        Select Case hoveredWord
-            Case "function" : Return "A keyword for identify current symbol is a function closure data object."
-            Case "as" : Return "A keyword for add type constraint decorating to the target symbol."
-            Case "let" : Return "A keyword for declare new symbol."
-            Case "const" : Return "A keyword for declare a new symbol that not mutable."
-            Case "string" : Return "R# character string type"
-            Case "integer" : Return "R# int64 type"
-            Case "double" : Return "R# float64 type"
-            Case "boolean" : Return "R# logical type"
-            Case "if" : Return "Execute the code closure based on the test condition is true or not."
-            Case "else" : Return "Execute the code in next closure if the given test condition is FALSE."
-            Case "using" : Return "A closure that will auto dispose the target symbol after finished the closure executation."
-            Case "for" : Return $"Loop through each elements in the given sequence."
-        End Select
-
-        Return DescriptionTooltip.GetDescription(hoveredWord)
-    End Function
-
-    ''' <summary>
-    ''' 顺序处于后面的优先级会很低
-    ''' </summary>
-    ''' <param name="sender"></param>
-    ''' <param name="e"></param>
-    Private Sub FastColoredTextBox1_TextChanged(sender As Object, e As TextChangedEventArgs) Handles FastColoredTextBox1.TextChanged
-        ' clear folding markers of changed range
-        e.ChangedRange.ClearFoldingMarkers()
-        ' set folding markers
-        e.ChangedRange.SetFoldingMarkers("{", "}")
-        e.ChangedRange.SetFoldingMarkers("\[", "\]")
-        e.ChangedRange.SetFoldingMarkers("\(", "\)")
-        e.ChangedRange.SetFoldingMarkers("""", """")
-        e.ChangedRange.SetFoldingMarkers("#region", "#endregion")
-
-        e.ChangedRange.ClearStyle(blue, green, red, endSymbol)
-        e.ChangedRange.SetStyle(link, "(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?")
-        e.ChangedRange.SetStyle(colorCode, htmlcolor)
-
-        e.ChangedRange.SetStyle(blue, keywords)
-        e.ChangedRange.SetStyle(blue, keyword2)
-        e.ChangedRange.SetStyle(blue, startKeyword)
-        e.ChangedRange.SetStyle(blue, "\sfrom\s")
-        e.ChangedRange.SetStyle(blue, functionKeyword)
-        e.ChangedRange.SetStyle(purple, buildInfunction)
-        e.ChangedRange.SetStyle(endSymbol, ";")
-        e.ChangedRange.SetStyle(orange, numbers)
-        e.ChangedRange.SetStyle(pipeLine, "[:]>")
-        e.ChangedRange.SetStyle(interpolate, stringInterpolate)
-        e.ChangedRange.SetStyle(funcCall, callFunc)
-
-        e.ChangedRange.SetStyle(red, "([""].*[""])|(['].*['])|([`].*[`])")
-        e.ChangedRange.SetStyle(green, "#.*")
-    End Sub
-
-    Private Function CharIsHyperlink(place As Place) As Boolean
-        Dim mask = FastColoredTextBox1.GetStyleIndexMask(New Style() {link})
-
-        If (place.iChar < FastColoredTextBox1.GetLineLength(place.iLine)) Then
-            If ((FastColoredTextBox1(place).style And mask) <> 0) Then
-                Return True
-            End If
-        End If
-
-        Return False
-    End Function
-
-    Private Sub FastColoredTextBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles FastColoredTextBox1.MouseMove
-        Dim p = FastColoredTextBox1.PointToPlace(e.Location)
-
-        If (CharIsHyperlink(p)) Then
-            FastColoredTextBox1.Cursor = Cursors.Hand
-        Else
-            FastColoredTextBox1.Cursor = Cursors.IBeam
-        End If
-    End Sub
-
-    Private Sub FastColoredTextBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles FastColoredTextBox1.MouseDown
-        Dim p = FastColoredTextBox1.PointToPlace(e.Location)
-
-        If (CharIsHyperlink(p)) Then
-            Dim url = FastColoredTextBox1.GetRange(p, p).GetFragment("[\S]").Text
-            Process.Start(url)
-        End If
-    End Sub
 End Class
