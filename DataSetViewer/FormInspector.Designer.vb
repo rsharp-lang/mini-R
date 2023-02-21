9cc8a621958d571aa1553d46497ed0575a3684b3
guigang xie <xie.guigang@gcmodeller.org>
2023-02-21 15:33:12 +0800

    try to load data via readRDS function

--- DataSetViewer/FormInspector.Designer.vb
+++ DataSetViewer/FormInspector.Designer.vb
@@ -22,10 +22,78 @@ Partial Class FormInspector
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        SplitContainer1 = New SplitContainer()
+        TreeView1 = New TreeView()
+        MenuStrip1.SuspendLayout()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.Panel1.SuspendLayout()
+        SplitContainer1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {OpenToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.Size = New Size(180, 22)
+        OpenToolStripMenuItem.Text = "Open"' 
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.Location = New Point(0, 24)
+        SplitContainer1.Name = "SplitContainer1"' 
+        ' SplitContainer1.Panel1
+        ' 
+        SplitContainer1.Panel1.Controls.Add(TreeView1)
+        SplitContainer1.Size = New Size(800, 426)
+        SplitContainer1.SplitterDistance = 266
+        SplitContainer1.TabIndex = 1
+        ' 
+        ' TreeView1
+        ' 
+        TreeView1.Dock = DockStyle.Fill
+        TreeView1.Location = New Point(0, 0)
+        TreeView1.Name = "TreeView1"
+        TreeView1.Size = New Size(266, 426)
+        TreeView1.TabIndex = 0
+        ' 
+        ' FormInspector
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(SplitContainer1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "FormInspector"
+        Text = "R# Data Inspector"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        SplitContainer1.Panel1.ResumeLayout(False)
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents TreeView1 As TreeView
 End Class
--- DataSetViewer/FormInspector.vb
+++ DataSetViewer/FormInspector.vb
@@ -1,3 +1,44 @@
-﻿Public Class FormInspector
+﻿Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
+Public Class FormInspector
+
+    ReadOnly R As RInterpreter = RInterpreter.Rsharp
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        Using file As New OpenFileDialog With {.Filter = "Any kinds(*.*)|*.*"}
+            If file.ShowDialog = DialogResult.OK Then
+                Text = $"Inspect[{file.FileName}]"
+                LoadFile(file.FileName)
+            End If
+        End Using
+    End Sub
+
+    Private Sub LoadFile(path As String)
+        Try
+            Dim value As Object = R.Evaluate($"readRDS('{path}');")
+
+            If value Is Nothing Then
+                MessageBox.Show("No data could be loaded!", "readRDS", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                Call LoadData(value)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Invalid data file format!", "readRDS", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub LoadData(x As Object)
+        Dim type As Type = x.GetType
+
+        Select Case type
+            Case GetType(vector)
+            Case GetType(list)
+            Case GetType(dataframe)
+        End Select
+    End Sub
+
+    Private Sub FormInspector_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
 End Class
