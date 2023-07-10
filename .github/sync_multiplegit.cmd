bd05fab705a8e452958e3dfd3ae3f65f7fb9d64c
xieguigang <gg.xie@bionovogene.com>
2023-07-10 16:36:08 +0800

    Create sync_multiplegit.cmd

---
+++ .github/sync_multiplegit.cmd
@@ -0,0 +1,14 @@
+@echo off
+
+REM git remote add gitee https://gitee.com/xieguigang/mini-R.git
+
+git pull gitee HEAD
+git push gitee HEAD
+
+REM git pull local HEAD
+REM REM git pull gitee HEAD
+
+REM git push local HEAD
+REM REM git push gitee HEAD
+
+echo synchronization of this code repository job done!
