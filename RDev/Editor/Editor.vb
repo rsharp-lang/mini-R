a0488c58fff0f302322a19e14e46b3a31d09c9f8
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-30 17:52:01 +0800

    fix of the reference error of the new css system

--- RDev/Editor/Editor.vb
+++ RDev/Editor/Editor.vb
@@ -1,6 +1,5 @@
-﻿Imports System.Drawing
+﻿Imports System.IO
 Imports System.Text
-Imports System.Windows.Forms
 Imports FastColoredTextBoxNS
 Imports Microsoft.VisualBasic.ComponentModel
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
@@ -56,6 +55,15 @@ Public Class Editor
 
     Public Function Save(path As String, encoding As Encoding) As Boolean Implements ISaveHandle.Save
         Return FastColoredTextBox1.Text.SaveTo(path, encoding)
+    End Function
+
+    Public Function Save(file As Stream, encoding As Encoding) As Boolean Implements ISaveHandle.Save
+        Using wr As New StreamWriter(file, encoding)
+            Call wr.WriteLine(FastColoredTextBox1.Text)
+            Call wr.Flush()
+        End Using
+
+        Return True
     End Function
 
     Public Function Save(path As String, Optional encoding As Encodings = Encodings.UTF8) As Boolean Implements ISaveHandle.Save
