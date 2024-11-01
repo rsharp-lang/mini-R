04b6ce17281f09fa3651549d015fe72409ab5880
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 15:30:14 +0800

    create basic editro framework

---
+++ Editor/Rstudio.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="vscode/monaco.d.ts" />
---
+++ Editor/Rstudio.js
@@ -0,0 +1,18 @@
+/// <reference path="../vscode/monaco.d.ts" />
+require.config({ paths: { vs: './vscode/min/vs' } });
+require(['vs/editor/editor.main'], function () {
+    var editor = monaco.editor.create(document.getElementById('container'), {
+        value: [
+            'imports "aaa" from "bbb";',
+            '',
+            'let f(x) = console.log("Hello world!");',
+            'let hello_world = function(x) {',
+            '   return `hello ${x}!`;',
+            '};',
+            '',
+            'print(c(1,2,3,4,5));'
+        ].join("\n"),
+        language: 'r'
+    });
+});
+//# sourceMappingURL=Rstudio.js.map
