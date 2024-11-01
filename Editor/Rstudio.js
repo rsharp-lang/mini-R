64fb2aac15bbabca2cc5aca540201ee2576e82b3
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 21:43:28 +0800

    implements of the custom language protocol

--- Editor/Rstudio.js
+++ Editor/Rstudio.js
@@ -4,6 +4,7 @@ var rstudio;
      * the language editor core
     */
     let editor;
+    let key;
     let demo_r = `    
 imports "JSON" from "base";
         
@@ -24,7 +25,8 @@ str(list(
     }
     rstudio.getCodeText = getCodeText;
     function create() {
-        let container = document.getElementById('container');
+        let container = $ts('#container');
+        key = md5(demo_r);
         editor = monaco.editor.create(container, {
             value: demo_r,
             language: 'r',
@@ -34,6 +36,8 @@ str(list(
                 enabled: monaco.editor.ShowLightbulbIconMode.On
             }
         });
+        // initialize of the server environment
+        lsp.put_script(demo_r, key);
     }
     rstudio.create = create;
     function setup() {
@@ -43,16 +47,150 @@ str(list(
         monaco.languages.registerCompletionItemProvider('r', {
             provideCompletionItems: (model, position) => rstudio.intellisense.create_intellisense(model, position)
         });
+        editor.onDidChangeModelContent((event) => {
+            // save to server
+            lsp.put_script(editor.getValue(), key);
+        });
     }
     rstudio.setup = setup;
 })(rstudio || (rstudio = {}));
 /// <reference path="../vscode/monaco.d.ts" />
+/// <reference path="../linq.d.ts" />
 /// <reference path="./editor.ts" />
+// run the vscode
 require.config({ paths: { vs: './vscode/min/vs' } });
 require(['vs/editor/editor.main'], function () {
     rstudio.setup();
     rstudio.create();
 });
+var lsp;
+(function (lsp) {
+    let ErrorCodes;
+    (function (ErrorCodes) {
+        // Defined by JSON-RPC
+        ErrorCodes.ParseError = -32700;
+        ErrorCodes.InvalidRequest = -32600;
+        ErrorCodes.MethodNotFound = -32601;
+        ErrorCodes.InvalidParams = -32602;
+        ErrorCodes.InternalError = -32603;
+        /**
+         * This is the start range of JSON-RPC reserved error codes.
+         * It doesn't denote a real error code. No LSP error codes should
+         * be defined between the start and end range. For backwards
+         * compatibility the `ServerNotInitialized` and the `UnknownErrorCode`
+         * are left in the range.
+         *
+         * @since 3.16.0
+         */
+        ErrorCodes.jsonrpcReservedErrorRangeStart = -32099;
+        /** @deprecated use jsonrpcReservedErrorRangeStart */
+        ErrorCodes.serverErrorStart = ErrorCodes.jsonrpcReservedErrorRangeStart;
+        /**
+         * Error code indicating that a server received a notification or
+         * request before the server has received the `initialize` request.
+         */
+        ErrorCodes.ServerNotInitialized = -32002;
+        ErrorCodes.UnknownErrorCode = -32001;
+        /**
+         * This is the end range of JSON-RPC reserved error codes.
+         * It doesn't denote a real error code.
+         *
+         * @since 3.16.0
+         */
+        ErrorCodes.jsonrpcReservedErrorRangeEnd = -32000;
+        /** @deprecated use jsonrpcReservedErrorRangeEnd */
+        ErrorCodes.serverErrorEnd = ErrorCodes.jsonrpcReservedErrorRangeEnd;
+        /**
+         * This is the start range of LSP reserved error codes.
+         * It doesn't denote a real error code.
+         *
+         * @since 3.16.0
+         */
+        ErrorCodes.lspReservedErrorRangeStart = -32899;
+        /**
+         * A request failed but it was syntactically correct, e.g the
+         * method name was known and the parameters were valid. The error
+         * message should contain human readable information about why
+         * the request failed.
+         *
+         * @since 3.17.0
+         */
+        ErrorCodes.RequestFailed = -32803;
+        /**
+         * The server cancelled the request. This error code should
+         * only be used for requests that explicitly support being
+         * server cancellable.
+         *
+         * @since 3.17.0
+         */
+        ErrorCodes.ServerCancelled = -32802;
+        /**
+         * The server detected that the content of a document got
+         * modified outside normal conditions. A server should
+         * NOT send this error code if it detects a content change
+         * in it unprocessed messages. The result even computed
+         * on an older state might still be useful for the client.
+         *
+         * If a client decides that a result is not of any use anymore
+         * the client should cancel the request.
+         */
+        ErrorCodes.ContentModified = -32801;
+        /**
+         * The client has canceled a request and a server has detected
+         * the cancel.
+         */
+        ErrorCodes.RequestCancelled = -32800;
+        /**
+         * This is the end range of LSP reserved error codes.
+         * It doesn't denote a real error code.
+         *
+         * @since 3.16.0
+         */
+        ErrorCodes.lspReservedErrorRangeEnd = -32800;
+    })(ErrorCodes = lsp.ErrorCodes || (lsp.ErrorCodes = {}));
+})(lsp || (lsp = {}));
+var lsp;
+(function (lsp) {
+    lsp.host = "";
+    function url(api, key) {
+        return `${lsp.host}${api}/?key=${key}`;
+    }
+    /**
+     * get symbol information
+     *
+     * @param document the key that reference to the specific script document inside the server memory
+     * @param offset the position offset on the script document
+     * @param symbol the symbol name for get the information
+    */
+    function get_symbol_info(document, offset, symbol) {
+        return fetch(url("/lsp/get/symbol", document)).then((response) => {
+        });
+    }
+    lsp.get_symbol_info = get_symbol_info;
+    /**
+     * put script text into server memory
+     *
+     * @param key a hash key that could be used for make reference of this script text
+    */
+    function put_script(script_str, key) {
+        let data = { doc: script_str };
+        $ts.post(url("/lsp/put", key), data, (response) => {
+        });
+    }
+    lsp.put_script = put_script;
+    /**
+     * commit the script document to the filesystem
+     *
+     * @param key a reference key that associated with a specific script text inside the server memory
+     * @param path the local file path for save the script file, must be a local full path
+    */
+    function commit(key, path) {
+        let data = { file: path };
+        $ts.post(url("/lsp/save", key), data, (response) => {
+        });
+    }
+    lsp.commit = commit;
+})(lsp || (lsp = {}));
 var rstudio;
 (function (rstudio) {
     var intellisense;
