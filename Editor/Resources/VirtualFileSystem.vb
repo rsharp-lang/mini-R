e0d38cd7bec5b30f05fbe7dcd027ead343d4c1f9
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 19:02:18 +0800

    get resource file

---
+++ Editor/Resources/VirtualFileSystem.vb
@@ -0,0 +1,19 @@
+﻿Imports System.IO
+Imports System.Resources
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.VisualBasic.ApplicationServices.Zip
+
+Public Module VirtualFileSystem
+
+    Const baseName = "vscode.vscode"
+    Const resourceName = "vscode"
+
+    Public Function GetHttpResourcesProvider() As IFileSystemEnvironment
+        Dim resourceManager As New ResourceManager(baseName, GetType(VirtualFileSystem).Assembly)
+        Dim zip As Stream = resourceManager.GetStream(resourceName)
+        Dim fs As New ZipStream(zip, is_readonly:=True)
+
+        Return fs
+    End Function
+
+End Module
