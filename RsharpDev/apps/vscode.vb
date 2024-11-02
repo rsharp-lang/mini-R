749be4bb5ef444c0b6560bedd0782e5cbd828531
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-02 18:23:15 +0800

    create of the background taskl

---
+++ RsharpDev/apps/vscode.vb
@@ -0,0 +1,19 @@
+﻿Imports Darwinism.HPC.Parallel
+Imports Microsoft.VisualBasic.ApplicationServices
+
+Public NotInheritable Class vscode
+
+    Private Sub New()
+    End Sub
+
+    Public Shared Function Launch() As Integer
+        Dim port As Integer = IPCSocket.GetFirstAvailablePort(delay:=0)
+        Dim rstudio As String = App.HOME & "/rstudio"
+        Dim rscript As String = $"{rstudio}/bin/Rscript.exe".GetFullPath
+        Dim languageserver As String = $"{rstudio}/R/languageserver.R".GetFullPath
+        Dim clr As String = $"{rstudio}/shares/vscode.dll".GetFullPath
+        Dim background As String = $"{rscript.CLIPath} {rscript.CLIPath} --port {port} --vscode {clr.CLIPath}"
+
+    End Function
+
+End Class
