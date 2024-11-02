158029ab47566e14e5f33935d88697e0232edbee
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-02 16:45:59 +0800

    make error fixed for the resource file

--- Editor/Resources/VirtualFileSystem.vb
+++ Editor/Resources/VirtualFileSystem.vb
@@ -10,7 +10,8 @@ Public Module VirtualFileSystem
 
     Public Function GetHttpResourcesProvider() As IFileSystemEnvironment
         Dim resourceManager As New ResourceManager(baseName, GetType(VirtualFileSystem).Assembly)
-        Dim zip As Stream = resourceManager.GetStream(resourceName)
+        Dim buffer As Byte() = resourceManager.GetObject(resourceName)
+        Dim zip As New MemoryStream(buffer)
         Dim fs As New ZipStream(zip, is_readonly:=True)
 
         Return fs
