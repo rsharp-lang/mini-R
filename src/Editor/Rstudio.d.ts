6087111369c0ef70512524fa909be0bc8c493b92
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-06 19:39:15 +0800

    update icons

--- src/Editor/Rstudio.d.ts
+++ src/Editor/Rstudio.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="vscode/monaco.d.ts" />
 /// <reference path="linq.d.ts" />
-declare namespace rstudio {
+declare module rstudio {
     function getCodeText(): string;
     function create(): void;
     function create_editor(script: string, lang: 'r' | 'json'): void;
@@ -242,7 +242,7 @@ declare namespace rstudio.intellisense {
     const r_primitive: string[];
     const r_const: string[];
 }
-declare namespace rstudio.tooltip {
+declare module rstudio.tooltip {
     function create_tooltip(model: monaco.editor.ITextModel, position: monaco.Position): any;
     function contentHtml(word: string): Promise<string>;
     const imports_keyword: string;
