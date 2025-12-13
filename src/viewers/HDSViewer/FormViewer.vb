0b26162c01f2b311abe58467c2200b122e70dafd
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-13 23:33:16 +0800

    set tree

--- src/viewers/HDSViewer/FormViewer.vb
+++ src/viewers/HDSViewer/FormViewer.vb
@@ -1,10 +1,12 @@
 ﻿Imports Galaxy.Data.JSON
+Imports Galaxy.Data.JSON.Models
 Imports Microsoft.VisualBasic.DataStorage.HDSPack.FileSystem
 
 Public Class FormViewer
 
     Dim WithEvents packTree As JsonViewer
     Dim pack As StreamPack
+    Dim filepath As String
 
     Private Sub FormViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         packTree = New JsonViewer
@@ -26,13 +28,27 @@ Public Class FormViewer
                     Call pack.Dispose()
                 End If
 
+                filepath = file.FileName
                 pack = StreamPack.OpenReadOnly(file.FileName)
+                Text = $"HDS Pack Viewer [{filepath}]"
+
                 LoadTree()
             End If
         End Using
     End Sub
 
     Private Sub LoadTree()
+        Dim tree As New JsonObject With {
+            .Id = filepath.FileName,
+            .JsonType = JsonType.Object,
+            .Value = pack.superBlock
+        }
+
+        Call LoadTree(tree, pack.superBlock)
+        Call packTree.Render(New JsonObjectTree(tree))
+    End Sub
+
+    Private Sub LoadTree(tree As JsonObject, group As StreamGroup)
 
     End Sub
 End Class
