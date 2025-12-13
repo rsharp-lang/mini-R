605361466832b0a6d8bc074e4e70cca3c91261ba
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-13 23:23:02 +0800

    add menu strip

--- src/viewers/HDSViewer/FormViewer.Designer.vb
+++ src/viewers/HDSViewer/FormViewer.Designer.vb
@@ -22,17 +22,46 @@ Partial Class FormViewer
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        SplitContainer1 = New SplitContainer()
+        MenuStrip1 = New MenuStrip()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.SuspendLayout()
         SuspendLayout()
         ' 
-        ' Form1
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.Location = New Point(0, 24)
+        SplitContainer1.Name = "SplitContainer1"
+        SplitContainer1.Size = New Size(942, 558)
+        SplitContainer1.SplitterDistance = 314
+        SplitContainer1.TabIndex = 0
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(942, 24)
+        MenuStrip1.TabIndex = 1
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FormViewer
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(942, 582)
-        Name = "Form1"
+        Controls.Add(SplitContainer1)
+        Controls.Add(MenuStrip1)
+        Name = "FormViewer"
         StartPosition = FormStartPosition.CenterScreen
         Text = "HDS Pack Viewer"
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents MenuStrip1 As MenuStrip
 
 End Class
--- src/viewers/HDSViewer/FormViewer.vb
+++ src/viewers/HDSViewer/FormViewer.vb
@@ -1,3 +1,13 @@
-﻿Public Class FormViewer
+﻿Imports Galaxy.Data.JSON
 
+Public Class FormViewer
+
+    Dim WithEvents packTree As JsonViewer
+
+    Private Sub FormViewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        packTree = New JsonViewer
+        packTree.Dock = DockStyle.Fill
+
+        Call SplitContainer1.Panel1.Controls.Add(packTree)
+    End Sub
 End Class
