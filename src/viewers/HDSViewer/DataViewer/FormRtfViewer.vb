2c440ac47e4e392e4d9abe64d5c6cb6eeb329991
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-12-13 23:51:16 +0800

    add blank viewer

---
+++ src/viewers/HDSViewer/DataViewer/FormRtfViewer.vb
@@ -0,0 +1,3 @@
+﻿Public Class FormRtfViewer
+
+End Class
---
+++ src/viewers/HDSViewer/FormExplorer.Designer.vb
@@ -0,0 +1,38 @@
+﻿Imports Galaxy.Workbench.DockDocument
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FormExplorer
+    Inherits ToolWindow
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' FormExplorer
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(341, 708)
+        Name = "FormExplorer"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+End Class
